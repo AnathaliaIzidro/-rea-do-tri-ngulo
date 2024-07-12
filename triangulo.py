a75094b1db5c9070848d202e3ca01994ec0d578f
Anathalia Izidro <49588396+AnathaliaIzidro@users.noreply.github.com>
2024-07-12 10:44:38 -0300

    Add files via upload

---
+++ triangulo.py
@@ -0,0 +1,15 @@
+# 2. Crie duas variáveis uma guarde o valor da base de um triangulo e outro guarde o valor da altura deste mesmo triangulo. Crie uma terceira variável para a area do triangulo e exiba a área usando o comando print.
+# area = base x altura / 2
+
+# base = 10
+# altura = 8
+# area = base * altura / 2
+# print("Área:", area)
+
+base = 10
+altura = 15
+
+perimetro = base * 3
+
+area = (base * altura) / 2
+print(area)
